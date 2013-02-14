ed90cad82fdee4d8c26495490e6109e6973d7bf3
makis <makis@makis-xubuntu.(none)>
2013-02-14 22:17:43 +0100

    empty rails project, kept SQLite to keep things simple

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sponsorpay::Application.config.secret_token = 'e65db2d46042e47049c7807fbfed3248641a59435f4f63e643b2a7a55b0cb3bbbb32009755eed02663b848b8e32fd67683e7686b417d819e5b07d3046902b888'
